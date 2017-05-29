bfeee54722625e5d68e18fb1e88e2ddfc8627da2
Tom <tom@tomforb.es>
2017-05-29 17:22:23 +0100

    Add some initial lxml based tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from xpath import Expression
+from lxml import etree
 
 
 @pytest.fixture
@@ -10,3 +11,19 @@ def expr():
 @pytest.fixture
 def value_expr():
     return Expression('1')
+
+
+@pytest.fixture
+def xml_root():
+    return Expression('/root')
+
+
+@pytest.fixture(scope='session')
+def xml_doc():
+    return etree.fromstring("""
+    <root abc='123'>
+        <!-- A comment -->
+        <node1 id='1'>Node1</node1>
+        <node2 id='2'>Node2</node2>
+    </root>
+    """)
---
+++ tests/test_lxml.py
@@ -0,0 +1,18 @@
+from xpath import Expression
+
+
+def test_root_node(xml_doc, xml_root):
+    assert xml_doc.xpath(str(xml_root.name)) == 'root'
+
+
+def test_root_node_attr(xml_doc, xml_root):
+    res = xml_doc.xpath(str(xml_root.attributes))
+    assert len(res) == 1
+
+
+def test_root_node_children(xml_doc, xml_root):
+    children = xml_doc.xpath(str(xml_root.children))
+    assert len(children) == 2
+
+    for child in xml_root.children(2):
+        assert xml_doc.xpath(str(child))
