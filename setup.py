2c8d78d125f0ca17d6170c2fd9f1f2680afeb69d
Tom <tom@tomforb.es>
2017-05-29 16:15:52 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='xpath-expressions',
+    version='0.1',
+    packages=['xpath'],
+    url='',
+    license='',
+    author='orf',
+    author_email='tom@tomforb.es',
+    description='Manipulate xpath expressions in pure python'
+)
